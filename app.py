b6a2597f589dd0765bf2b09f39a36d993679fc3d
ekaputra04 <ekaputrajuniawan@gmail.com>
2024-05-09 22:10:15 +0800

    just for fun

--- app.py
+++ app.py
@@ -1,26 +1,16 @@
 import os
 import random
-import subprocess
 
-# Ganti dengan informasi repositori Anda
 repository_url = "https://github.com/ekaputra04/commit-generator.git"
-# commit_author = "Your Name <your.email@example.com>"
-commit_message = "Fake commit message"
-
-# Clone repositori
-# os.system(f"git clone {repository_url}")
-# os.chdir("commit-generator")
+commit_message = "just for fun"
 
 jumlah_perulangan = random.randint(1, 5)
 
 for _ in range(jumlah_perulangan):
-# Tambahkan perubahan ke file atau buat file baru
   with open("note.txt", "a+") as file:
-      file.write("This is a fake file.\n")
+      file.write("Just for fun :).\n")
 
-  # Lakukan commit
   os.system("git add .")
   os.system(f'git commit  -m "{commit_message}"')
 
-# Push perubahan ke repositori
 os.system("git push -u origin main")
